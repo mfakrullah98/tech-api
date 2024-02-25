cd747a0897d9d3046386514074c98eea607d1abf
Fakrul <fakrullah@thebooking360.com>
2024-02-26 06:20:42 +0800

    refactor: remove unnecessary item

--- app/Http/Resources/ApiResource.php
+++ app/Http/Resources/ApiResource.php
@@ -15,19 +15,12 @@ class ApiResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->id,
             'country' => $this->country,
-            'code'=> $this->code,
             '2010'=>$this->y2010,
             '2011'=>$this->y2011,
             '2012'=>$this->y2012,
             '2013'=>$this->y2013,
             '2014'=>$this->y2014,
-            'sum'=>$this->y2010+$this->y2011+$this->y2012+$this->y2013+$this->y2014,
-            'average'=>($this->y2010+$this->y2011+$this->y2012+$this->y2013+$this->y2014)/5,
-            'median'=>($this->y2010+$this->y2011+$this->y2012+$this->y2013+$this->y2014)/2,
-            'max'=>max($this->y2010,$this->y2011,$this->y2012,$this->y2013,$this->y2014),
-            'min'=>min($this->y2010,$this->y2011,$this->y2012,$this->y2013,$this->y2014),
         ];
     }
 }
